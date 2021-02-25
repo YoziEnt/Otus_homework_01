86c0a18a4220c0ec1e45adb67469898421657d86
Oleg Mishkin <o.mishkin@squaregps.com>
2021-02-26 00:01:14 +0500

    Initial Commit

---
+++ Homework_01/Homework_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_01App.swift
+//  Homework_01
+//
+//  Created by Олег on 26.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Homework_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
